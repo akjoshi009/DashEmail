5373f03ffd8414eb16fb4918673f608ac8997a55
Sachin Anbhule <31981860+akjoshi009@users.noreply.github.com>
2020-03-17 20:12:23 +0530

    Update dashfile.py

--- dashfile.py
+++ dashfile.py
@@ -1,33 +1,67 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-
-
+    html.H1(children='Email Classifier',style={"margin-left":"20px","text-allign":"center"}),
     dcc.Input(
-    placeholder='Enter a value...',
+    placeholder='Enter a Subject...',
     type='text',
-    value='') ,
+    value='',
+    id='subject',
+    style={
+                "width": "50%",
+                "height": "60px",
+                "textAlign": "left",
+                "margin": "10px"
+            }
+    ) ,
     
    dcc.Textarea(
-    placeholder='Enter a value...',
-    value='This is a TextArea component',
-    style={'width': '100%'}
+    placeholder='Enter a Body...',
+    value='',
+    id="body",
+    style={'width': '50%',
+           "margin": "10px"
+           }
 )
-   ,html.Button('Check Mailclass', id='button')
+   ,html.Button('Submit', id='button',
+                style={"textAlign": "center",
+                       "margin": "10px",
+                       'width': '50%'
+                       }
+                ),
+    html.H1(children='',id="output",style={"margin-left":"20px","text-allign":"center"})
    
-])
+],style={"margin-left":"30%","margin-top":"10%"})
+   
+
+@app.callback(
+    Output("output", "children"),
+    [Input("subject", "value"), Input("body", "value"),Input(component_id='button', component_property='n_clicks')]
+)
+def loadtomodel(sub,body,n_clicks):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        return geteclass(sub+body)
+
+
+
+def geteclass(text):
+    #load model from here and predict
+    return "Not Spam"
+
+      
+   
 
 if __name__ == '__main__':
    app.run_server()
